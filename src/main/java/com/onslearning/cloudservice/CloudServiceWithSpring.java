8f9e98df4ca1aec57699e5036e671d3ef6c32e33
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-08-23 15:21:44 -0600

    Initial commit

---
+++ src/main/java/com/onslearning/cloudservice/CloudServiceWithSpring.java
@@ -0,0 +1,13 @@
+package com.onslearning.cloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServiceWithSpring {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServiceWithSpring.class, args);
+    }
+
+}
---
+++ src/test/java/com/onslearning/cloudservice/CloudServiceWithSpringTests.java
@@ -0,0 +1,13 @@
+package com.onslearning.cloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceWithSpringTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
